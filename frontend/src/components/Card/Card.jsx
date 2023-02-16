01146f4f0132c279f52341764bd4f21ab6ddb627
Victor Feliciano <victorjfeliciano@gmail.com>
2023-02-16 14:18:54 -0300

    primeiras modicicacoes usando react

---
+++ frontend/src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+        <div className="card">
+            <h1>Esse é o card bom</h1>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Six-handed_500_playing_cards.jpg/250px-Six-handed_500_playing_cards.jpg" alt="summoner skull card" />
+        </div>
+    )
+}
+
+export default Card;
