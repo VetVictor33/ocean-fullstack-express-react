f08d2a0b9fa4b92a0964464b2a60977ac9aca6e3
Victor Feliciano <victorjfeliciano@gmail.com>
2023-02-23 12:21:20 -0300

    removendo comentarios e add instruções para exercicio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,18 @@ import './App.css';
 import ReadAll from './components/ReadAll/ReadAll';
 import Header from './components/Header/Header';
 
+/**
+ * Novos desafios!
+ *
+ * Tecnologias: ReactJS; Criação de componente; UseState; UseEffect.
+ * 1. Criar um componente ReadById
+ * 2. Dentro do componente, realizar uma requisição para o endpoint de Read by ID no backend
+ * 3. Pegar as informações do backend e enviar para o componente Card
+ *
+ * Dica: Pode ocultar o componente ReadAll enquanto tiver construindo o ReadById, para não
+ * se confundir nas requisições
+ */
+
 function App() {
   return (
     <div>
