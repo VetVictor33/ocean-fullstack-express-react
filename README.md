70953e436da1caa3f99da0bea54f779d07816802
Victor Feliciano <victorjfeliciano@gmail.com>
2023-02-24 09:34:34 -0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ocean-fullstack-express-react
+# Jornada Fullstack Ocean
+
+Projeto de criação de um app full stack (backend e frontend) lecionado pelo professor Paulo Salvatore
--- frontend/src/components/ReadById/ReadById.jsx
+++ frontend/src/components/ReadById/ReadById.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 function ReadById() {
 
-    const [item, setItem] = (useState[]);
+    //const [item, setItem] = (useState[]);
     async function request(id) {
         const url = `http://localhost:3000/itens/${id}`;
         const response = await fetch(url);
@@ -12,9 +12,9 @@ function ReadById() {
         setItem(data)
     }
 
-    document.querySelector('#getItem').addEventListener('click', () => {
+    /*document.querySelector('#getItem').addEventListener('click', () => {
 
-    })
+    })*/
 
 
     return (
